3641ec9efb37b9d59f335d5482d556a7e389297b
Ben Alman <cowboy@rj3.net>
2010-08-03 00:38:43 +0000

    jQuery each2: fixed a pretty major bug. Should *actually* work now.

--- jquery.ba-each2.js
+++ jquery.ba-each2.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery each2 - v0.1 - 8/02/2010
+ * jQuery each2 - v0.2 - 8/02/2010
  * http://benalman.com/projects/jquery-misc-plugins/
  * 
  * Inspired by James Padolsey's quickEach
@@ -12,11 +12,26 @@
 
 (function($) {
   
-  var jq = $([]);
+  // Create a placeholder jQuery object with a length of 1. The single item
+  // is completely arbitrary and will be replaced.
+  var jq = $([1]);
   
   $.fn.each2 = function( fn ) {
     var i = -1;
-    while ( ( jq[0] = this[++i] ) && fn.call( jq[0], i, jq ) !== false ) {}
+    
+    while (
+      // Set both the first element AND context property of the placeholder
+      // jQuery object to the DOM element. When i > this.length, this[++i]
+      // will return undefined and abort the while loop.
+      ( jq.context = jq[0] = this[++i] )
+      
+      // Invoke the callback function in the context of the DOM element,
+      // passing both the index and the placeholder jQuery object in. Like
+      // .each, if the callback returns `false`, abort the while loop.
+      && fn.call( jq[0], i, jq ) !== false
+    ) {}
+    
+    // Return the initial jQuery object for chainability.
     return this;
   };
   
